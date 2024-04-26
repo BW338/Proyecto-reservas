ddf66699384e1897dae19d615655594a56cfd42e
Guido <guidoamadio88@gmail.com>
2024-04-26 11:08:43 -0300

    Configurando Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYBsRgO1RRAwVmhyI4byoX3lYev3DM2A8",
+  authDomain: "reservas-5a992.firebaseapp.com",
+  projectId: "reservas-5a992",
+  storageBucket: "reservas-5a992.appspot.com",
+  messagingSenderId: "462599081210",
+  appId: "1:462599081210:web:3fa82efdeb4a663fa70d43",
+  measurementId: "G-6081BT79YB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Elimina la llamada a getAnalytics
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -9,6 +28,26 @@ function App() {
   const [showModal, setShowModal] = useState(false);
   const [selectedHour, setSelectedHour] = useState(null);
   const [isReservationCancelled, setIsReservationCancelled] = useState(false);
+  const today = new Date();
+
+  useEffect(() => {
+    // Consulta los horarios de reserva de Firestore al cargar la aplicación
+    const fetchData = async () => {
+      const dateFormatted = selectedDate.toISOString().split('T')[0];
+      const docRef = doc(db, "horarios", dateFormatted);
+      const docSnap = await getDocs(docRef);
+      if (docSnap.exists()) {
+        setDatesWithTimes(docSnap.data());
+      } else {
+        // Si no hay datos para la fecha seleccionada, inicializa los horarios de reserva como vacíos
+        setDatesWithTimes({});
+      }
+    };
+
+    if (selectedDate) {
+      fetchData();
+    }
+  }, [selectedDate]);
 
   const handleDayPress = (date) => {
     if (selectedDate && date.toISOString() === selectedDate.toISOString()) {
@@ -18,47 +57,44 @@ function App() {
     }
   };
 
-  const handleTimeSlotClick = (hour) => {
+  const handleTimeSlotClick = async (hour) => {
+    if (!selectedDate) return;
+  
     const dateString = selectedDate.toISOString().split('T')[0];
-    const timesForDate = datesWithTimes[dateString] || [];
-    if (timesForDate.includes(hour)) {
+    const docRef = doc(db, "horarios", dateString);
+    const docSnap = await getDoc(docRef);
+  
+    if (docSnap.exists()) {
+      const timesForDate = docSnap.data()[dateString] || [];
       setShowModal(true);
       setSelectedHour(hour);
-      setIsReservationCancelled(true);
-    } else {
-      setShowModal(true);
-      setSelectedHour(hour);
-      setIsReservationCancelled(false);
+      setIsReservationCancelled(timesForDate.includes(hour));
     }
   };
+  
 
-  const handleConfirmReservation = () => {
+  const handleConfirmReservation = async () => {
     setShowModal(false);
     const dateString = selectedDate.toISOString().split('T')[0];
-    const timesForDate = datesWithTimes[dateString] || [];
+    let updatedTimesForDate = { ...datesWithTimes };
+  
     if (isReservationCancelled) {
-      setDatesWithTimes({
-        ...datesWithTimes,
-        [dateString]: timesForDate.filter((time) => time !== selectedHour),
-      });
+      updatedTimesForDate[dateString] = updatedTimesForDate[dateString].filter((time) => time !== selectedHour);
     } else {
-      if (!timesForDate.includes(selectedHour)) {
-        setDatesWithTimes({
-          ...datesWithTimes,
-          [dateString]: [...timesForDate, selectedHour],
-        });
-      }
+      updatedTimesForDate[dateString] = [...(updatedTimesForDate[dateString] || []), selectedHour];
     }
+  
+    await updateDoc(doc(db, "horarios", dateString), updatedTimesForDate);
+    setDatesWithTimes(updatedTimesForDate);
   };
+  
 
   const renderTimeSlots = () => {
     const columns = [[], [], []];
     if (selectedDate) {
-      const dateString = selectedDate.toISOString().split('T')[0];
-      const selectedTimes = datesWithTimes[dateString] || [];
       for (let hour = 9; hour <= 23; hour++) {
         const column = Math.floor((hour - 9) / 5);
-        const isSelected = selectedTimes.includes(hour);
+        const isSelected = (datesWithTimes[selectedDate.toISOString().split('T')[0]] || []).includes(hour);
         columns[column] = columns[column] || [];
         columns[column].push(
           <div
@@ -85,7 +121,11 @@ function App() {
       </header>
       <main className='cuerpo'>
         <div className="calendar-container">
-          <Calendar value={selectedDate} onClickDay={handleDayPress} />
+          <Calendar
+            value={selectedDate}
+            onClickDay={handleDayPress}
+            minDate={today}
+          />
         </div>
         {selectedDate && (
           <div className="time-slots-container">
@@ -101,8 +141,8 @@ function App() {
           <div className="modal-content">
             <p>
               {isReservationCancelled
-                ? `¿Quieres cancelar la reserva del horario ${selectedHour}:00 del día ${selectedDate.toLocaleDateString('es-ES', { weekday: 'long' })} ${selectedDate.getDate()} de ${selectedDate.toLocaleDateString('es-ES', { month: 'long' })}?`
-                : `¿Quieres reservar el horario ${selectedHour}:00 del día ${selectedDate.toLocaleDateString('es-ES', { weekday: 'long' })} ${selectedDate.getDate()} de ${selectedDate.toLocaleDateString('es-ES', { month: 'long' })}?`}
+                ? <>¿Quieres <strong>cancelar</strong> la reserva del horario {selectedHour}:00 del día {selectedDate.toLocaleDateString('es-ES', { weekday: 'long' })} {selectedDate.getDate()} de {selectedDate.toLocaleDateString('es-ES', { month: 'long' })}?</>
+                : <>¿Quieres <strong>reservar</strong> el horario {selectedHour}:00 del día {selectedDate.toLocaleDateString('es-ES', { weekday: 'long' })} {selectedDate.getDate()} de {selectedDate.toLocaleDateString('es-ES', { month: 'long' })}?</>}
             </p>
             <button onClick={handleConfirmReservation}>Sí</button>
             <button onClick={() => setShowModal(false)}>No</button>
@@ -120,3 +160,5 @@ function App() {
 }
 
 export default App;
+
+
