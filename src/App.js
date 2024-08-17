923fea45ffbbbdc43e6cc4e1bc4cc86a8a561638
Guido <guidoamadio88@gmail.com>
2024-08-17 10:57:45 -0300

    Firebase removed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBYBsRgO1RRAwVmhyI4byoX3lYev3DM2A8",
-  authDomain: "reservas-5a992.firebaseapp.com",
-  projectId: "reservas-5a992",
-  storageBucket: "reservas-5a992.appspot.com",
-  messagingSenderId: "462599081210",
-  appId: "1:462599081210:web:3fa82efdeb4a663fa70d43",
-  measurementId: "G-6081BT79YB"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Elimina la llamada a getAnalytics
-// const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -30,64 +11,41 @@ function App() {
   const [isReservationCancelled, setIsReservationCancelled] = useState(false);
   const today = new Date();
 
-  useEffect(() => {
-    // Consulta los horarios de reserva de Firestore al cargar la aplicación
-    const fetchData = async () => {
-      const dateFormatted = selectedDate.toISOString().split('T')[0];
-      const docRef = doc(db, "horarios", dateFormatted);
-      const docSnap = await getDocs(docRef);
-      if (docSnap.exists()) {
-        setDatesWithTimes(docSnap.data());
-      } else {
-        // Si no hay datos para la fecha seleccionada, inicializa los horarios de reserva como vacíos
-        setDatesWithTimes({});
-      }
-    };
-
-    if (selectedDate) {
-      fetchData();
-    }
-  }, [selectedDate]);
-
   const handleDayPress = (date) => {
     if (selectedDate && date.toISOString() === selectedDate.toISOString()) {
       setSelectedDate(null);
     } else {
       setSelectedDate(date);
+      const dateFormatted = date.toISOString().split('T')[0];
+      const timesForDate = datesWithTimes[dateFormatted] || [];
+      setDatesWithTimes({ ...datesWithTimes, [dateFormatted]: timesForDate });
     }
   };
 
-  const handleTimeSlotClick = async (hour) => {
+  const handleTimeSlotClick = (hour) => {
     if (!selectedDate) return;
-  
-    const dateString = selectedDate.toISOString().split('T')[0];
-    const docRef = doc(db, "horarios", dateString);
-    const docSnap = await getDoc(docRef);
-  
-    if (docSnap.exists()) {
-      const timesForDate = docSnap.data()[dateString] || [];
-      setShowModal(true);
-      setSelectedHour(hour);
-      setIsReservationCancelled(timesForDate.includes(hour));
-    }
-  };
-  
 
-  const handleConfirmReservation = async () => {
+    const dateString = selectedDate.toISOString().split('T')[0];
+    const timesForDate = datesWithTimes[dateString] || [];
+
+    setShowModal(true);
+    setSelectedHour(hour);
+    setIsReservationCancelled(timesForDate.includes(hour));
+  };
+
+  const handleConfirmReservation = () => {
     setShowModal(false);
     const dateString = selectedDate.toISOString().split('T')[0];
     let updatedTimesForDate = { ...datesWithTimes };
-  
+
     if (isReservationCancelled) {
       updatedTimesForDate[dateString] = updatedTimesForDate[dateString].filter((time) => time !== selectedHour);
     } else {
       updatedTimesForDate[dateString] = [...(updatedTimesForDate[dateString] || []), selectedHour];
     }
-  
-    await updateDoc(doc(db, "horarios", dateString), updatedTimesForDate);
+
     setDatesWithTimes(updatedTimesForDate);
   };
-  
 
   const renderTimeSlots = () => {
     const columns = [[], [], []];
@@ -160,5 +118,3 @@ function App() {
 }
 
 export default App;
-
-
